c185b44437bed7103859a2424bb8af7cc7729ab5
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2017-09-20 17:56:08 +1000

    stats added

--- Analysis/both/testConUncon.m
+++ Analysis/both/testConUncon.m
@@ -4,20 +4,22 @@ load('HCP_GenCog_Group.mat')
 % load distance uncorrected coexpression
 load('averageCoexpressionHCPuncorrected.mat')
 
-Adj = logical(HCP(1:180,1:180)); 
+% do same for GenCog
+Adj = logical(GenCog(1:180,1:180)); 
 con = averageCoexpression(Adj==1); 
 uncon = averageCoexpression(Adj==0); 
 dataCell{1} = con; 
 dataCell{2} = uncon; 
-
+[t,p,stat,v] = ttest2(con, uncon)
 JitteredParallelScatter(dataCell); 
 
+% do same for HCP
 Adj = logical(HCP(1:180,1:180)); 
 con = averageCoexpression(Adj==1); 
 uncon = averageCoexpression(Adj==0); 
 dataCell{1} = con; 
 dataCell{2} = uncon; 
-
+[t,p,stat,v] = ttest2(con, uncon)
 JitteredParallelScatter(dataCell); 
 
 % load distance corrected data
