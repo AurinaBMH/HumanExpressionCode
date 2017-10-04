722b3a4c44b9168192bcd338c8d45a5742f36b66
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2017-10-04 12:10:18 +1100

    script to test coexpression for connected vs unconnected links in distance bins

---
+++ Analysis/both/coexpConUnconBins.m
@@ -0,0 +1,52 @@
+% compare coexpression for connected vs unconnected links
+cd ('data/connectomes')
+load('HCPgroupConnectomes.mat')
+cd ..
+cd ('genes/processedData')
+load('100DScorrected.mat')
+load('HCPdistances.mat')
+
+Adj = double(logical(AdjCount(1:180, 1:180)));
+
+
+%% ---------------------------------------------------------------------------------
+% filter data in bins
+[xThresholds,yMeans] = BF_PlotQuantiles(distPlot(:),averageCoexpression(:),10,false,true);
+
+nanMat = nan(180,180);
+nanMat = tril(nanMat);
+nanMat(nanMat==0) = 1;
+
+distPlot = distPlot.*nanMat;
+averageCoexpression = averageCoexpression.*nanMat;
+
+% bin data into quantiles
+%% ---------------------------------------------------------------------------------
+figure; 
+
+for i=2:length(xThresholds)
+ 
+    ind = distPlot<=xThresholds(i) & distPlot>xThresholds(i-1);
+    
+    % map onto connectivity data
+    maskCon = Adj & ind;
+    maskUncon = ~Adj & ind;
+    
+    coexpCon = nonzeros(averageCoexpression.*maskCon);
+    coexpCon(isnan(coexpCon)) = [];
+    coexpUncon = nonzeros(averageCoexpression.*maskUncon);
+    coexpUncon(isnan(coexpUncon)) = [];
+    
+    dataCell{1} = coexpCon;
+    dataCell{2} = coexpUncon;
+    [h,p(i),ci,stats] = ttest2(dataCell{1}, dataCell{2});
+    subplot(3,3,i-1); 
+    JitteredParallelScatter(dataCell, true, true, false);
+    title(sprintf('Connected VS unconnected in bin %d', i-1));
+    ylabel('Coexpression');
+    set(gca,'Xtick', [1 2], 'XTickLabel',{sprintf('Connected (%d pairs)', length(coexpCon)), ...
+    sprintf('Unconnected (%d pairs)',length(coexpUncon))}); 
+
+end  
+cd ../../..
+        
