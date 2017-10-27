80c68b780c3b8645b95e16c542f5ef2a675ad327
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2017-10-27 23:37:51 +1100

    reoders probes

--- Analysis/genes/DScorrelation.m
+++ Analysis/genes/DScorrelation.m
@@ -1,25 +1,40 @@
-load('DSvariance.mat') % - generated using S5 script (probes chosen based on variance)
-DSscores{1} = DS; probeSelection{1} = 'variance'; 
+load('DSnewVariance.mat') % - generated using S5 script (probes chosen based on variance)
+[a1,order] = sort(probeInformation.EntrezID); 
+DSscores{1} = DS(order); probeSelection{1} = 'variance'; 
+allData{1} = expSampNormalisedAll(:,order); 
 
-load('DSpc.mat') % - generated using S5 script (probes chosen based on PC)
-DSscores{2} = DS; probeSelection{2} = 'pc'; 
+load('DSnewPC.mat') % - generated using S5 script (probes chosen based on PC)
+[a2,order] = sort(probeInformation.EntrezID); 
+DSscores{2} = DS(order); probeSelection{2} = 'PC'; 
+allData{2} = expSampNormalisedAll(:,order); 
 
-load('DSnoise.mat')
-DSscores{3} = DS; probeSelection{3} = 'noise'; 
 
-load('DSmean.mat');
-DSscores{4} = DS; probeSelection{4} = 'mean'; 
+load('DSnewLessNoise.mat')
+[a3,order] = sort(probeInformation.EntrezID); 
+DSscores{3} = DS(order); probeSelection{3} = 'noise'; 
+allData{3} = expSampNormalisedAll(:,order); 
+
+
+load('DSnewMean.mat');
+[a4,order] = sort(probeInformation.EntrezID); 
+DSscores{4} = DS(order); probeSelection{4} = 'Mean'; 
+allData{4} = expSampNormalisedAll(:,order); 
+
+load('DSnewrandom.mat');
+[a5,order] = sort(probeInformation.EntrezID); 
+DSscores{5} = DS(order); probeSelection{5} = 'random'; 
+allData{5} = expSampNormalisedAll(:,order); 
 
 
 sz=10; 
 figure; title ('Correlation between DS scores'); 
-r = zeros(4,4); 
-p = zeros(4,4); 
+r = zeros(5,5); 
+p = zeros(5,5); 
 f=1; 
-for i=1:4
-    for j=i+1:4
+for i=1:5
+    for j=i+1:5
 
-        subplot(2,3,f); scatter(DSscores{i}, DSscores{j}, sz, 'filled');
+        subplot(2,5,f); scatter(DSscores{i}, DSscores{j}, sz, 'filled');
         [r(i,j),p(i,j)] = corr(DSscores{i}', DSscores{j}', 'type', 'Spearman'); 
         xlabel(sprintf('DS for probes selected based on %s', probeSelection{i})); 
         ylabel(sprintf('DS for probes selected based on %s', probeSelection{j}));
