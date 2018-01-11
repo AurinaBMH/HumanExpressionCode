1ad0a2561f5c08b5aa2461a9521c9b40cddcca40
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2018-01-11 17:37:08 +1100

    more outputs included

--- dataProcessing/genes/selectProbeRNAseq.m
+++ dataProcessing/genes/selectProbeRNAseq.m
@@ -1,5 +1,5 @@
 % function to select probe based on RNA seq values
-function indProbe = selectProbeRNAseq(DataTable, EntrezID, indKeepProbes, threshold)
+function [correlations, avgCorr, indProbe] = selectProbeRNAseq(DataTable, EntrezID, indKeepProbes, threshold)
 
 numGenes = length(unique(EntrezID));
 correlations = cell(numGenes,2);
